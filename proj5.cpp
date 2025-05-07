0b7e2435a319496402ab05add73e8e6a3da4a66e
Soham Metha <sohammetha01@gmail.com>
2025-05-07 23:49:03 +0000

    08/05/2025 : 05:19:03

--- proj5.cpp
+++ proj5.cpp
@@ -14,7 +14,7 @@ class Node
     Node(int k, string v, Node *n) : key(k), value(v), next(n) {};
     void display()
     {
-        cout << " { " << key << " ━━━ " << value << "}";
+        cout << " { " << key << "\t: " << value << "}";
     }
 };
 
