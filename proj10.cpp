c4953166b6fda69737865e3df4a1472043e8cb5b
Soham Metha <sohammetha01@gmail.com>
2025-05-08 03:12:24 +0000

    08/05/2025 : 08:42:24

--- proj10.cpp
+++ proj10.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
+class Node
+{
     int val;
     Node *left, *right;
 
-    public:
-    Node() {
+  public:
+    Node()
+    {
         val = 0;
         left = right = NULL;
     }
 
-    Node(int d) {
+    Node(int d)
+    {
         val = d;
         left = right = NULL;
     }
@@ -19,117 +22,128 @@ class Node {
     friend class Heap;
 };
 
-class Heap {
+class Heap
+{
     Node *root;
     int numNodes;
 
-    public:
-    Heap() {
+  public:
+    Heap()
+    {
         root = NULL;
         numNodes = 0;
     }
 
-    void insert(int keys[], int n) {
+    void insert(int keys[], int n)
+    {
         numNodes = n;
         Node *newNode = new Node(keys[0]);
         root = newNode;
 
-        //bfs insert
-        queue<Node*> que;
+        // bfs insert
+        queue<Node *> que;
         int i = 0;
         que.push(root);
-        while(i < n-1) {   //IMP: as i is incremented twice inside the loop
+        while (i < n - 1)
+        {
 
             Node *poppedNode = que.front();
             que.pop();
 
-            //for the left child
             Node *leftNode = new Node(keys[++i]);
-            poppedNode -> left = leftNode;
-            que.push(poppedNode -> left);
+            poppedNode->left = leftNode;
+            que.push(poppedNode->left);
 
-            //for the right child
             Node *rightNode = new Node(keys[++i]);
-            poppedNode -> right = rightNode;
-            que.push(poppedNode -> right);
+            poppedNode->right = rightNode;
+            que.push(poppedNode->right);
         }
     }
 
-    Node* getLastNode() {
-        queue<Node*> que;
+    Node *getLastNode()
+    {
+        queue<Node *> que;
         que.push(root);
         Node *poppedNode = NULL;
-        while(!que.empty()) {
+        while (!que.empty())
+        {
             poppedNode = que.front();
             que.pop();
 
-            if(poppedNode -> left) que.push(poppedNode -> left);
-            if(poppedNode -> right) que.push(poppedNode -> right);
-
+            if (poppedNode->left)
+                que.push(poppedNode->left);
+            if (poppedNode->right)
+                que.push(poppedNode->right);
         }
         return poppedNode;
     }
-    
-    Node* heapify(Node *node) {                     //bit tricky due to recursion
-        if(node == NULL) {
+
+    Node *heapify(Node *node)
+    {
+        if (node == NULL)
             return NULL;
-        }
 
-        node -> left = heapify(node -> left);       // Heapify left subtree
-        node -> right = heapify(node -> right);     // Heapify right subtree
+        node->left = heapify(node->left);
+        node->right = heapify(node->right);
 
-        if(node -> left != NULL && node -> left -> val > node -> val) {
-            int temp = node -> left -> val;
-            node -> left -> val = node -> val;
-            node -> val = temp;
+        if (node->left != NULL && node->left->val > node->val)
+        {
+            int temp = node->left->val;
+            node->left->val = node->val;
+            node->val = temp;
         }
-        if(node -> right != NULL && node -> right -> val > node -> val) {
-            int temp = node -> right -> val;
-            node -> right -> val = node -> val;
-            node -> val = temp;
+        if (node->right != NULL && node->right->val > node->val)
+        {
+            int temp = node->right->val;
+            node->right->val = node->val;
+            node->val = temp;
         }
         return node;
     }
 
-    Node* deleteNode(Node *node, int key) {
-        if(node == NULL )
+    Node *deleteNode(Node *node, int key)
+    {
+        if (node == NULL)
             return NULL;
-        
-        if(node -> val == key) {
+
+        if (node->val == key)
+        {
             delete node;
             return NULL;
         }
 
-        node -> left = deleteNode(node -> left, key);
-        node -> right = deleteNode(node -> right, key);
-        
+        node->left = deleteNode(node->left, key);
+        node->right = deleteNode(node->right, key);
+
         return node;
     }
 
-    void sort() {
+    void sort()
+    {
         cout << "Descending Order: " << endl;
-        for(int i=0; i<numNodes; i++) {
+        for (int i = 0; i < numNodes; i++)
+        {
             heapify(root);
 
             Node *lastNode = getLastNode();
 
-            //swap root node's value with the last node's value
-            int temp = root -> val;
-            root -> val = lastNode -> val;
-            lastNode -> val = temp;
+            // swap root node's value with the last node's value
+            int temp = root->val;
+            root->val = lastNode->val;
+            lastNode->val = temp;
 
-            //Print the value
-            cout << lastNode -> val << " ";
+            // Print the value
+            cout << lastNode->val << " ";
 
-            //delete the node
-            deleteNode(root, lastNode -> val);
+            // delete the node
+            deleteNode(root, lastNode->val);
         }
         cout << endl;
     }
 };
 
-
-int main() {
+int main()
+{
 
     Heap heap;
     int keys[] = {87, 5, 34, 99, 44, 12, 68};
