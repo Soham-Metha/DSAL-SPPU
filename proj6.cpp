40450abae59b0d1c2fa0353ba51fcdb588d1f45d
Soham Metha <sohammetha01@gmail.com>
2025-05-08 00:16:58 +0000

    08/05/2025 : 05:46:58

--- proj6.cpp
+++ proj6.cpp
@@ -1,158 +1,178 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
+class Node
+{
     string name;
     Node *next;
     Node *down;
 
-    public: 
-    Node(string str) {
+  public:
+    Node(string str)
+    {
         name = str;
         next = down = NULL;
     }
 
     friend class Graph;
-}; 
+};
 
-
-class Graph {
+class Graph
+{
     Node *head = NULL;
 
-    void addNode(string nodeA, string nodeB) {
+    void addNode(string nodeA, string nodeB)
+    {
         add(nodeA, nodeB);
         add(nodeB, nodeA);
     }
 
-    public:
-    void add(string nodeA, string nodeB) {
-        //If adjacency list is empty
-        if(head == NULL) {
+  public:
+    void add(string nodeA, string nodeB)
+    {
+        // If adjacency list is empty
+        if (head == NULL)
+        {
             Node *node1 = new Node(nodeA);
             Node *node2 = new Node(nodeB);
-            node1 -> next = node2;
+            node1->next = node2;
             head = node1;
             return;
         }
-        
-        Node *currNode = head;
+
+        Node *currNode;
         Node *prev = NULL;
         bool found = false;
-        while(currNode != NULL) {
-            //If nodeA is found
-            if(currNode -> name == nodeA) {
-                found = true;
-                Node *currListNode = currNode;
-                while(currListNode -> next != NULL) {
-                    currListNode = currListNode -> next;
-                }
-                Node *node2 = new Node(nodeB);
-                currListNode -> next = node2;
-                break;
+        for (currNode = head; currNode != NULL; currNode = currNode->down)
+        {
+            if (currNode->name != nodeA)
+            {
+                prev = currNode;
+                continue;
             }
-            prev = currNode;
-            currNode = currNode -> down;
+            found = true;
+            Node *currListNode;
+            for (currListNode = currNode; currListNode->next != NULL; currListNode = currListNode->next)
+                ;
+            Node *node2 = new Node(nodeB);
+            currListNode->next = node2;
+            break;
         }
-        //If nodeA is not found
-        if(!found) {
+        // If nodeA is not found
+        if (!found)
+        {
             Node *node1 = new Node(nodeA);
             Node *node2 = new Node(nodeB);
-            prev -> down = node1;
-            node1 -> next = node2;
+            prev->down = node1;
+            node1->next = node2;
         }
     }
 
-    void print() {
-        Node *currArrayNode = head;
-        while(currArrayNode != NULL) {
-            Node *currListNode = currArrayNode;
-            while(currListNode != NULL) {
-                cout << currListNode -> name << " -> ";
-                currListNode = currListNode -> next;
+    void print()
+    {
+        for (Node *currArrayNode = head; currArrayNode != NULL; currArrayNode = currArrayNode->down)
+        {
+            for (Node *currListNode = currArrayNode; currListNode != NULL; currListNode = currListNode->next)
+            {
+                cout << currListNode->name << " -> ";
             }
-            cout << " NULL ";
-            currArrayNode = currArrayNode -> down;
-            cout << endl;
+            cout << " NULL " << endl;
         }
     }
 
-    void bfs(string startNode) {
+    void bfs(string startNode)
+    {
         vector<string> visited;
         queue<string> Queue;
         string poppedNode;
         visited.push_back(startNode);
         Queue.push(startNode);
-        while(true) {
-            if(!Queue.empty()) {
+        while (true)
+        {
+            if (!Queue.empty())
+            {
                 poppedNode = Queue.front();
                 Queue.pop();
                 cout << poppedNode << " ";
             }
-            else {
+            else
+            {
                 cout << endl;
                 break;
             }
 
-            Node *currNode = head;
             bool found = false;
-            while(currNode != NULL) {
-                if(currNode -> name == poppedNode) {
+            for (Node *currNode = head; currNode != NULL; currNode = currNode->down)
+
+                if (currNode->name == poppedNode)
+                {
                     found = true;
-                    Node *currListNode = currNode -> next;
-                    while(currListNode != NULL) {
-                        if( find(visited.begin(), visited.end(), currListNode -> name) == visited.end() ) {
-                            visited.push_back(currListNode -> name);
-                            Queue.push(currListNode -> name);
+                    Node *currListNode = currNode->next;
+                    while (currListNode != NULL)
+                    {
+                        if (find(visited.begin(), visited.end(), currListNode->name) == visited.end())
+                        {
+                            visited.push_back(currListNode->name);
+                            Queue.push(currListNode->name);
                         }
-                        currListNode = currListNode -> next;
+                        currListNode = currListNode->next;
                     }
                     break;
                 }
-                currNode = currNode -> down; 
-            }
-            if(!found) {
+
+            if (!found)
+            {
                 cout << "Node is not present in the graph" << endl;
                 return;
             }
         }
     }
 
-    void dfs(string start) {
+    void dfs(string start)
+    {
         vector<string> visited;
         stack<string> Stack;
         string poppedNode;
         visited.push_back(start);
         Stack.push(start);
-        while(true) {
-            
-            if(!Stack.empty()) {
+        while (true)
+        {
+
+            if (!Stack.empty())
+            {
                 poppedNode = Stack.top();
                 Stack.pop();
                 cout << poppedNode << " ";
             }
-            else {
+            else
+            {
                 cout << endl;
                 break;
             }
-            
+
             bool found = false;
             Node *currNode = head;
-            while(currNode != NULL) {
-                if(currNode -> name == poppedNode) {
+            while (currNode != NULL)
+            {
+                if (currNode->name == poppedNode)
+                {
                     found = true;
-                    Node *currListNode = currNode -> next;
-                    while(currListNode != NULL) {
-                        if( find(visited.begin(), visited.end(), currListNode -> name) == visited.end() ){
-                           visited.push_back(currListNode -> name);
-                           Stack.push(currListNode -> name);  
+                    Node *currListNode = currNode->next;
+                    while (currListNode != NULL)
+                    {
+                        if (find(visited.begin(), visited.end(), currListNode->name) == visited.end())
+                        {
+                            visited.push_back(currListNode->name);
+                            Stack.push(currListNode->name);
                         }
-                        currListNode = currListNode -> next;
+                        currListNode = currListNode->next;
                     }
                     break;
                 }
-                currNode -> down;
+                currNode->down;
             }
-            if(!found) {
+            if (!found)
+            {
                 cout << "Node does not exist" << endl;
                 break;
             }
@@ -160,8 +180,8 @@ class Graph {
     }
 };
 
-
-int main() {
+int main()
+{
     Graph g;
     g.add("A", "B");
     g.add("A", "C");
