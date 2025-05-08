4dbd4b3946f90392e2ecc4faedd2964f49455e4e
Soham Metha <sohammetha01@gmail.com>
2025-05-08 01:06:42 +0000

    08/05/2025 : 06:36:42

--- proj6.cpp
+++ proj6.cpp
@@ -38,9 +38,9 @@ void Graph::DFS(string start)
 
     list<string>::iterator i;
     for (i = adjList[start].begin(); i != adjList[start].end(); ++i)
-        if (!visited[*i])
-            //DFS(*i);
+        if (!visited[*i]){
             cout << *i;
+            DFS(*i);}
 }
 
 void Graph::BFS(string s)
